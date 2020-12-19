4990edc04d0d56f1a8a3550734f52bd2de61503e
Gareth <Gareth.kh.lau@gmail.com>
2020-12-18 20:44:42 -0700

    Prevent search without providing title

--- client/src/_queries/useMovies.js
+++ client/src/_queries/useMovies.js
@@ -7,15 +7,19 @@ export default function useMovies(title) {
     () =>
       new Promise(
         (resolve, reject) => {
-          axios
-            .get("/api/movie?title=" + title)
-            .then((response) => {
-              const movies = response.data?.movies;
-              resolve(movies);
-            })
-            .catch((error) => {
-              reject(error);
-            });
+          if (title?.trim().length === 0) {
+            resolve([]);
+          } else {
+            axios
+              .get("/api/movie?title=" + title)
+              .then((response) => {
+                const movies = response.data?.movies;
+                resolve(movies);
+              })
+              .catch((error) => {
+                reject(error);
+              });
+          }
         },
         {
           retry: false,
