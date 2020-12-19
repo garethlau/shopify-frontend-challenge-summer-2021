98b90b8ad99145948abcd9496becd5bde0745ea1
Gareth <Gareth.kh.lau@gmail.com>
2020-12-18 23:58:22 -0700

    Prevent horizontal scrolling

--- client/src/_pages/Ballot.js
+++ client/src/_pages/Ballot.js
@@ -114,142 +114,144 @@ export default function Nominate() {
   }, [ballot]);
 
   return (
-    <div className={classes.root}>
-      <div className={classes.container}>
-        <h1>
-          <CopyToClipboard
-            text={`${ORIGIN}/ballot/${ballotId}`}
-            onCopy={() => {
-              enqueueSnackbar(`URL copied to clipboard.`, {
-                variant: "success",
-              });
-              setTimeout(closeSnackbar, 5000);
-            }}
-          >
-            <span className={classes.link}>
-              Ballot: {ballotId} <LinkIcon />
-            </span>
-          </CopyToClipboard>
-        </h1>
-        <QRCode />
+    <div style={{ overflowX: "hidden" }}>
+      <div className={classes.root}>
+        <div className={classes.container}>
+          <h1>
+            <CopyToClipboard
+              text={`${ORIGIN}/ballot/${ballotId}`}
+              onCopy={() => {
+                enqueueSnackbar(`URL copied to clipboard.`, {
+                  variant: "success",
+                });
+                setTimeout(closeSnackbar, 5000);
+              }}
+            >
+              <span className={classes.link}>
+                Ballot: {ballotId} <LinkIcon />
+              </span>
+            </CopyToClipboard>
+          </h1>
+          <QRCode />
 
-        <div>
-          {isLoading ? (
-            "Loading"
-          ) : (
-            <div className={classes.ballot}>
-              <h1>Nominations</h1>
-              {nominated.length === 5 ? (
-                <div className={classes.completeBanner}>
-                  <p>Congratulations, you've nominated 5 movies!</p>
-                </div>
-              ) : (
-                <p>
-                  You've nominated {nominated.length} movie
-                  {nominated.length !== 1 && "s"}, {5 - nominated.length} more
-                  to go!
-                </p>
-              )}
-              {nominated.map((movie, index) => {
-                return (
-                  <MovieCard
-                    key={index}
-                    title={movie.Title}
-                    poster={movie.Poster}
-                    year={movie.Year}
-                    action={
-                      <Button
-                        onClick={async () => {
-                          await nominateMovie({
-                            imdbID: movie.imdbID,
-                            action: "withdraw",
-                          });
-                          refetchBallot();
-                        }}
-                        variant="contained"
-                        color="secondary"
-                      >
-                        Remove
-                      </Button>
-                    }
-                  />
-                );
-              })}
-            </div>
-          )}
-        </div>
-
-        <h1>Search</h1>
-        <input
-          className={classes.search}
-          value={search.value}
-          onChange={search.onChange}
-          placeholder="I'm looking for ..."
-        />
-        <div>
-          {debouncedSearch && movies?.length === 0 && <p>No results.</p>}
-          {moviesStatus !== "idle" && moviesStatus === "loading" && (
-            <p>Loading</p>
-          )}
-          <AnimatePresence>
-            {movies?.pages?.map((page, pageNumber) => (
-              <React.Fragment key={pageNumber}>
-                {page.movies?.map((movie, index) => (
-                  <MovieCard
-                    key={index}
-                    title={movie.Title}
-                    year={movie.Year}
-                    poster={movie.Poster}
-                    action={
-                      <Button
-                        disabled={nominated
-                          .map((x) => x.imdbID)
-                          .includes(movie.imdbID)}
-                        variant="contained"
-                        color="primary"
-                        onClick={async () => {
-                          if (nominated.length < 5) {
+          <div>
+            {isLoading ? (
+              "Loading"
+            ) : (
+              <div className={classes.ballot}>
+                <h1>Nominations</h1>
+                {nominated.length === 5 ? (
+                  <div className={classes.completeBanner}>
+                    <p>Congratulations, you've nominated 5 movies!</p>
+                  </div>
+                ) : (
+                  <p>
+                    You've nominated {nominated.length} movie
+                    {nominated.length !== 1 && "s"}, {5 - nominated.length} more
+                    to go!
+                  </p>
+                )}
+                {nominated.map((movie, index) => {
+                  return (
+                    <MovieCard
+                      key={index}
+                      title={movie.Title}
+                      poster={movie.Poster}
+                      year={movie.Year}
+                      action={
+                        <Button
+                          onClick={async () => {
                             await nominateMovie({
                               imdbID: movie.imdbID,
-                              action: "nominate",
+                              action: "withdraw",
                             });
                             refetchBallot();
-                          } else {
-                            enqueueSnackbar(
-                              "You've already nominated 5 movies.",
-                              { variant: "error" }
-                            );
-                          }
-                        }}
-                      >
-                        Nominate
-                      </Button>
-                    }
-                  />
-                ))}
-              </React.Fragment>
-            ))}
-          </AnimatePresence>
-          <AnimatePresence>
-            {hasNextPage && !isFetchingNextPage && (
-              <motion.div
-                initial={{ opacity: 0, y: 100, scale: 0.5 }}
-                animate={{ opacity: 1, y: 0, scale: 1 }}
-                exit={{ opacity: 0, y: 100, scale: 0.5 }}
-                transition={{ delay: 0 }}
-              >
-                <Button
-                  fullWidth
-                  variant="contained"
-                  color="secondary"
-                  onClick={fetchNextPage}
-                  disabled={!hasNextPage || isFetchingNextPage}
-                >
-                  Load More
-                </Button>
-              </motion.div>
+                          }}
+                          variant="contained"
+                          color="secondary"
+                        >
+                          Remove
+                        </Button>
+                      }
+                    />
+                  );
+                })}
+              </div>
             )}
-          </AnimatePresence>
+          </div>
+
+          <h1>Search</h1>
+          <input
+            className={classes.search}
+            value={search.value}
+            onChange={search.onChange}
+            placeholder="I'm looking for ..."
+          />
+          <div>
+            {debouncedSearch && movies?.length === 0 && <p>No results.</p>}
+            {moviesStatus !== "idle" && moviesStatus === "loading" && (
+              <p>Loading</p>
+            )}
+            <AnimatePresence>
+              {movies?.pages?.map((page, pageNumber) => (
+                <React.Fragment key={pageNumber}>
+                  {page.movies?.map((movie, index) => (
+                    <MovieCard
+                      key={index}
+                      title={movie.Title}
+                      year={movie.Year}
+                      poster={movie.Poster}
+                      action={
+                        <Button
+                          disabled={nominated
+                            .map((x) => x.imdbID)
+                            .includes(movie.imdbID)}
+                          variant="contained"
+                          color="primary"
+                          onClick={async () => {
+                            if (nominated.length < 5) {
+                              await nominateMovie({
+                                imdbID: movie.imdbID,
+                                action: "nominate",
+                              });
+                              refetchBallot();
+                            } else {
+                              enqueueSnackbar(
+                                "You've already nominated 5 movies.",
+                                { variant: "error" }
+                              );
+                            }
+                          }}
+                        >
+                          Nominate
+                        </Button>
+                      }
+                    />
+                  ))}
+                </React.Fragment>
+              ))}
+            </AnimatePresence>
+            <AnimatePresence>
+              {hasNextPage && !isFetchingNextPage && (
+                <motion.div
+                  initial={{ opacity: 0, y: 100, scale: 0.5 }}
+                  animate={{ opacity: 1, y: 0, scale: 1 }}
+                  exit={{ opacity: 0, y: 100, scale: 0.5 }}
+                  transition={{ delay: 0 }}
+                >
+                  <Button
+                    fullWidth
+                    variant="contained"
+                    color="secondary"
+                    onClick={fetchNextPage}
+                    disabled={!hasNextPage || isFetchingNextPage}
+                  >
+                    Load More
+                  </Button>
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </div>
         </div>
       </div>
     </div>
