8195f6702fcbc4cc3a24590e1e54e3e9a9af5dc2
Gareth <Gareth.kh.lau@gmail.com>
2020-12-20 11:40:19 -0700

    Persist dark mode setting to local storage

--- client/src/_contexts/darkMode.js
+++ client/src/_contexts/darkMode.js
@@ -2,14 +2,36 @@ import { useState, useEffect, createContext } from "react";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { lightTheme, darkTheme } from "../themes";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 export const DarkModeContext = createContext();
 
 export const DarkModeProvider = ({ children }) => {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  useEffect(() => {
+    let savedDarkModePreference = localStorage.getItem("prefers-dark-mode");
+    if (savedDarkModePreference === null) {
+      if (prefersDarkMode) {
+        setIsDarkMode(true);
+      }
+    } else {
+      setIsDarkMode(savedDarkModePreference);
+    }
+  }, []);
+
+  useEffect(() => {
+    function persistDarkMode() {
+      localStorage.setItem("prefers-dark-mode", isDarkMode);
+    }
+    window.addEventListener("beforeunload", persistDarkMode);
+    return () => {
+      window.removeEventListener("beforeunload", persistDarkMode);
+    };
+  }, []);
 
   function toggle() {
-    console.log("Is Dark Mode? ", isDarkMode);
     setIsDarkMode(!isDarkMode);
   }
 
