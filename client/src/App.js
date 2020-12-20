8b319cb3048c6b9ed77bb1324cb9494103b42166
Gareth <Gareth.kh.lau@gmail.com>
2020-12-20 11:31:57 -0700

    Remove unused imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,10 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { ThemeProvider } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import { darkTheme, lightTheme } from "./themes";
-
+import { SnackbarProvider } from "notistack";
+import { DarkModeProvider } from "./_contexts/darkMode";
 import Home from "./_pages/Home";
 import Ballot from "./_pages/Ballot";
 import Font from "./_pages/Font";
-import { SnackbarProvider } from "notistack";
-import { DarkModeProvider } from "./_contexts/darkMode";
 
 const queryClient = new QueryClient();
 
