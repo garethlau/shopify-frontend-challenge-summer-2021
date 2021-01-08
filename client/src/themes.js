88d37b56125e006fbbabccecdea1ee3a3f0faa88
Gareth <Gareth.kh.lau@gmail.com>
2021-01-08 14:58:02 -0700

    Use Google Font CDN

--- client/src/themes.js
+++ client/src/themes.js
@@ -1,21 +1,5 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
-import CourgetteRegularTtf from "./_fonts/Courgette-Regular.ttf";
-
-const courgette = {
-  fontFamily: "Courgette",
-  fontStyle: "normal",
-  fontDisplay: "swap",
-  fontWeight: 400,
-  src: `
-    local('Courgette'),
-    local('Courgette-Regular'),
-    url(${CourgetteRegularTtf}) format('truetype')
-  `,
-  unicodeRange:
-    "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF",
-};
-
 const base = {
   palette: {
     primary: {
@@ -35,14 +19,12 @@ const base = {
     },
   },
   typography: {
-    fontFamily: "Courgette, Arial",
+    fontFamily: ["Open Sans", "sans-serif"],
     fontSize: 18,
-  },
-  overrides: {
-    MuiCssBaseline: {
-      "@global": {
-        "@font-face": [courgette],
-      },
+    h1: {
+      fontFamily: ["Courgette", "cursive"],
+      fontSize: "2em",
+      margin: "10px",
     },
   },
 };
