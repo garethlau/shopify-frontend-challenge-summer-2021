899f3b8abe511219dd7a26d66998f47da9c68836
Gareth <Gareth.kh.lau@gmail.com>
2021-01-08 18:13:28 -0700

    Cache movie requests for 1 hour

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const apicache = require("apicache");
+
+const cache = apicache.middleware;
 
 router.get("/health", (_, res) => {
   return res.status(200).send({ message: "Server is healthy." });
 });
+
 router.use("/api/ballots", require("./ballots"));
-router.use("/api/movies", require("./movies"));
+router.use("/api/movies", cache("1 hour"), require("./movies"));
 
 module.exports = router;
